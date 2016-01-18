0b22aa6c88887cd918e06253d1d5d547616e516f
Mikael Lima <mkmikael07@gmail.com>
2016-01-18 10:47:43 -0300

    Mudanca nos inserts

--- src/main/java/br/com/ufra/docs/insertsIniciais.sql
+++ src/main/java/br/com/ufra/docs/insertsIniciais.sql
@@ -1,2 +1,5 @@
-insert into tecnico values(null, 'jns@email.com','001', 'Jairo Sousa','ROLE_ADMIN', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
+insert into tecnico values(null, '001','Jairo Sousa', 'jns@email.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','ROLE_ADMIN');
+insert into tecnico values(null, '002','Geovane', 'geo@email.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','ROLE_ADMIN');
+
+
 
