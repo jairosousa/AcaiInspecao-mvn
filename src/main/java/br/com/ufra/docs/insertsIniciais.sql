f7c3887cab1d96e9c8dd4e5640cbc46d7121fba4
Jairo Sousa <Jairo Sousa@10.11.83.2>
2016-01-20 19:34:51 -0300

    Mudança no Layoute do formulário cadastrar estabelecimento

--- src/main/java/br/com/ufra/docs/insertsIniciais.sql
+++ src/main/java/br/com/ufra/docs/insertsIniciais.sql
@@ -1,2 +1,2 @@
-insert into tecnico values(null, 'jns@email.com','001', 'Jairo Sousa', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','ROLE_ADMIN');
+insert into tecnico values(null, 'jns@email.com','001', 'Jairo Sousa','ROLE_ADMIN', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
