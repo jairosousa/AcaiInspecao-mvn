59cca5385bd1a9ddaaf3dda4592a1dbf44bc3a66
acaipaidegua <appacaipaidegua@gmail.com>
2016-01-21 20:31:43 -0200

    Mudança no Layout parte 1.

--- src/main/java/br/com/ufra/spring/LoginSpring.java
+++ src/main/java/br/com/ufra/spring/LoginSpring.java
@@ -63,6 +63,6 @@ public class LoginSpring implements UserDetailsService {
     }
 
     public static void main(String[] args) {
-        System.out.println(encode("123"));
+        System.out.println(encode("jnssls"));
     }
 }
